7f1c0b15f7b3604e6b25ebf1f92e2445412ccef3
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2025-02-04 21:13:33 +0530

    Create High Price of Products

---
+++ High Price of Products.sql
@@ -0,0 +1,4 @@
+-- Write a query to find all product_name and category that have a price greater than 100.00 from the Products table.
+SELECT product_name, category
+FROM Products
+WHERE price > 100.00;
